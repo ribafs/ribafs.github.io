d0183249ce6711a5c649b3e1055c914816ba9b86
Ribamar FS <ribafs@gmail.com>
2021-06-26 15:39:41 -0300

    Update

--- professor/apps/Joguinhos/SuperMarioBros/includes/get.js
+++
@@ -1,8 +0,0 @@
-//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-/*
-Esta funcion devuelve el objeto con el id pasado por argumento.
-*/
-//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-function get(d){
-	return document.getElementById(d);
-}
